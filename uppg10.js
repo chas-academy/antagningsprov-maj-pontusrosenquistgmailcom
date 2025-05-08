c4ded1ebd0bb667cbefd5cab497e1d8f5996a1c0
pontus.rosenquist@gmail.com <pontus.rosenquist@gmail.com>
2025-05-08 19:38:04 +0200

    C16 u10

--- uppg10.js
+++ uppg10.js
@@ -2,12 +2,34 @@ function uppg10(){
     const numbers = [5, 12, 7, 10, 20, 9, 11];
   
     // skapa tre arrayer - bigEven, smallOdd, other
+    let bigEven = []
+    let smallOdd =[]
+    let other = []
+
+    
     // loopa igenom numbers och placera talen i respektive array
-    // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
-    // smallOdd ska innehålla tal mindre än 10 och udda
-    // other ska innehålla alla andra tal
+    for(num of numbers){
+      
+      // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
+        if(num >10 && num % 2 === 0){
+          bigEven.push(num)
+        }
+
+      // smallOdd ska innehålla tal mindre än 10 och udda
+        else if (num < 10 && num %2 !== 0){
+          smallOdd.push(num)
+        }
+
+      // other ska innehålla alla andra tal
+        else{
+          other.push(num)
+        }
+    }
+
+    let returnObj = {bigEven, smallOdd, other}
     // returnera ett objekt med de tre arrayerna
 
+    return returnObj
   
     
   

6e96cb6c43ef8120682eae336bb63d69677319cb
pontus.rosenquist@gmail.com <pontus.rosenquist@gmail.com>
2025-05-08 18:11:43 +0200

    C13

--- uppg9.js
+++ uppg9.js
@@ -5,21 +5,22 @@ function uppg9 (){
 
     function sort (numbers){
         for(number of numbers){
+            // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
             if(number % 2 === 0){
-                console.log(Number(number) + "jämt")
+                console.log(`${Number(number)} jämt`)
             }
             else{
-                console.log(Number(number) + "udda")
+                console.log(`${Number(number)} jämt`)
             }
         }
     }
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
+    
     // i if-sattsen skriv ut i konsolen resultatet
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
 
     // anropa funktionen och skicka med en array som argument
 
-    let myNumbers = [1,17,8,33,34,100,74]
+    let myNumbers = [13,17,8,33,34,100,74]
 
     sort(myNumbers)
   
